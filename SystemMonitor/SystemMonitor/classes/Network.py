314ba9cd6ba2abfa50fe36e8185579ef30361a37
Aaron Baker <crosby.baker2010@gmail.com>
2019-06-16 23:38:10 -0500

    Changes made to the network class so that there is a section that is printing out the information about how much of a change there has been in the packet information since the last time the system was checked

--- SystemMonitor/SystemMonitor/classes/Network.py
+++ SystemMonitor/SystemMonitor/classes/Network.py
@@ -7,7 +7,8 @@ from .Converters import *
 class Network:
 
     networkIO = None
-    networkSentBytesDifferent = []
+    networkBytes = {"Sent" : [], "Received" : []}
+    networkPackets = {"Sent" : [], "Received" : []}
 
     def __init__(self):
         self.networkIO = psutil.net_io_counters()
@@ -38,11 +39,26 @@ class Network:
         print(" -------------------------------------------------------------------------------")
         print(" -                            Network I/O (Changes)                            -")
         print(" -------------------------------------------------------------------------------")
+        try:
+            print("\tBytes Sent:\t%s\t\tBytes Received:\t%s" % (self.networkBytes["Sent"][-1] - self.networkBytes["Sent"][-2],self.networkBytes["Received"][-1] - self.networkBytes["Received"][-2]))
+        except:
+            print("\t\tNo Changes yet. Not enough information yet (Bytes Sent/Received)")
 
+        try:
+            print("\tPackets Sent:\t%s\t\tPackets Received:\t%s" % (self.networkPackets["Sent"][-1] - self.networkPackets["Sent"][-2],self.networkPackets["Received"][-1] - self.networkPackets["Received"][-2]))
+        except:
+            print("\t\tNo Changes yet. Not enough information yet (Packets Sent/Received)")
         print(" -------------------------------------------------------------------------------")
         return
 
     def Update_Print(self):
-        self.networkIO = psutil.net_io_counters()
+        self.networkBytes["Sent"].append(self.networkIO.bytes_sent)
+        self.networkBytes["Received"].append(self.networkIO.bytes_recv)
+        self.networkPackets["Sent"].append(self.networkIO.packets_sent)
+        self.networkPackets["Received"].append(self.networkIO.packets_recv)
+
         self.Pretty_Print_Network_IO()
+        self.Print_Change()
+        
+        self.networkIO = psutil.net_io_counters()
         return
